b2048614d9b3573c97cbc8f6eb0513cc5f1a9b07
ravipilgar <65125744+ravipilgar@users.noreply.github.com>
2021-07-19 09:52:01 +0530

    Done commit on Solution.txt

---
+++ Second.java
@@ -0,0 +1,5 @@
+public class Passion{
+     public static void main(String[] args){
+          System.out.println("Git And GitHub");
+     }
+}
---
+++ Third.java
@@ -0,0 +1,5 @@
+public class Location{
+      public static void main(String[] args){
+            System.out.println("Software Department");
+      }
+}
